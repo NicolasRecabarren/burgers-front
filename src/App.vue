0aea391480c5c69282fa11e67dc9b3f9d0183bc2
NicolasRecabarrenV <recabarren.valderrama.nicolas@gmail.com>
2020-08-25 00:32:07 -0400

    La navegación del sitio se mueve a un componente.

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,21 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/hamburguesas">Hamburguesas</router-link>
+    <Navbar/>
+    <div class="container">
+      <router-view/>
     </div>
-    <router-view/>
   </div>
 </template>
+
+<script>
+import Navbar from './components/Navbar.vue';
+
+export default {
+  components: {
+    Navbar,
+  },
+};
+</script>
 
 <style lang="scss">
 #app {

3a45709dbc171cfa6dd60f3bfa5cd3df94d41e42
NicolasRecabarrenV <recabarren.valderrama.nicolas@gmail.com>
2020-08-25 00:31:08 -0400

    - Se agrega CDN de bootstrap para utilizar en la aplicación. - SE agrega plugin bootstrap-vue para utilizar los componentes de bootstrap.

--- src/main.js
+++ src/main.js
@@ -1,9 +1,21 @@
 import VueResource from 'vue-resource';
 import Vue from 'vue';
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
+
 import App from './App.vue';
 import router from './router';
 
+// Estilos para componentes de plugin bootstrap-vue
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+
+Vue.use(BootstrapVue); // Le indicamos a Vue que utilizaremos el plugin de bootstrap-vue.
+Vue.use(IconsPlugin); // Le indicamos a Vue que utilizaremos los iconos de bootstrap-vue.
+
 Vue.use(VueResource);
+
+
 Vue.config.productionTip = false;
 
 new Vue({
